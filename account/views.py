21a2b70619ced87b46ebc90c081905a21c535ac1
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-04-06 12:28:01 +0300

    add user registration functionality

--- account/views.py
+++ account/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .forms import CreateUserForm
+from django.http import HttpResponse
+
 
 
 def home(request):
@@ -8,7 +11,27 @@ def home(request):
 
 
 def register(request):
-    return render(request, 'account/register.html')
+
+
+    form=CreateUserForm()
+    if request.method=='POST':
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('user registered')
+        
+
+    context={'RegisterForm':form}
+
+    return render(request, 'account/register.html',context)
+
+
+
+
+
+
+
+
 
 
 def my_login(request):
