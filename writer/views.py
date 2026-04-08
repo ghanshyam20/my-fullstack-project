2477ce858f1d89abc87c7f1e9fbffda9855c3559
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-04-09 01:32:18 +0300

    update article views

--- writer/views.py
+++ writer/views.py
@@ -53,6 +53,28 @@ def my_articles(request):
     return render(request,'writer/my-articles.html',context)
 
 
+
+@login_required(login_url='my-login')
+def update_article(request,pk):
+    article=Article.objects.get(id=pk)
+
+    form=ArticleForm(instance=article)
+
+    if request.method=='POST':
+        form=ArticleForm(request.POST,instance=article)
+
+
+        if form.is_valid:
+            form.save()
+
+            return redirect('my-articles')
+        
+
+    context={'UpdateArticleForm':form}
+
+    return render(request,'writer/update-article.html',context)
+
+
     
         
 
