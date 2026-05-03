fe4b0de978a8a697aae859a0bbd1cb54a6bb9d2d
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-05-03 22:13:45 +0300

    updated urls for writer

--- writer/urls.py
+++ writer/urls.py
@@ -11,5 +11,5 @@ urlpatterns=[
     path('update-article/<str:pk>',views.update_article,name='update-article'),
     path('delete-article/<str:pk>',views.delete_article,name='delete-article'),
     path('delete-image/<int:pk>/', views.delete_image, name='delete-image'),
-    path('account-management',views.account_management,name='account-management'),
+    #path('account-management',views.account_management,name='account-management'),
     ]
