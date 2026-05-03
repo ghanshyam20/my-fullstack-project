2949c5f629a7ce16e2af74e8f44f0ebefba7430d
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-05-04 02:48:45 +0300

    fixed writer auth

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.mail import send_mail
 from django.conf import settings
 
-from django.conf import settings
+
 
 from . managers import CustomUsermanager
 from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin
